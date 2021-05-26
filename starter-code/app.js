e0cb6d39adf6dccddb967db4fd1d1eb110f0243a
Kenneth Paige <kpwebdevelopment@gmail.com>
2021-05-26 05:42:34 -0400

    styled section 2 added section 3 and styled section 3

---
+++ starter-code/app.js
@@ -0,0 +1,15 @@
+$("#familyGathering1").click(function changeText(){
+    $("#familyGatheringHeader").html("Family Gathering")
+    $("#familyGatheringParagraph").html("We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We'll provide a memorable experience for all.")
+})
+
+
+$("#familyGathering2").click(function changeText(){
+    $("#familyGatheringHeader").html("Special Events")
+    $("#familyGatheringParagraph").html("Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you We’ll be sure to mark your special date with an unforgettable meal.")
+})
+
+$("#familyGathering3").click(function changeText(){
+    $("#familyGatheringHeader").html("Social Events")
+    $("#familyGatheringParagraph").html("Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone")
+})
