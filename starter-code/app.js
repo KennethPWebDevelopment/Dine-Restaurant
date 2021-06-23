a6ae0f04ee1fdb99a6b9da1bba57d1660d98a8f2
Kenneth Paige <kpwebdevelopment@gmail.com>
2021-06-23 03:17:13 -0400

    Refactor desktop homepage

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,34 +2,41 @@
 
 // SECTION 3 SLIDE
 
-$('#familyGathering1, #familyGathering2, #familyGathering3').click(function () {
-    if (this.id == 'familyGathering1') {
-        $("#familyGatheringHeader").html("Family Gathering")
-        $("#familyGatheringParagraph").html("We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We'll provide a memorable experience for all.")
-        $("#familyGatheringImg").attr("src", "images/homepage/family-gathering-desktop.jpg")
-        $(this).css('color', 'black');
-        $('#familyGathering2').css('color', '#4C4C4C');
-        $('#familyGathering3').css('color', '#4c4c4c');
-        $("#section3HR").css({top: '-2%', right: '107%', position:'absolute'});
-    }
-    else if (this.id == 'familyGathering2') {
-        $("#familyGatheringHeader").html("Special Events")
-        $("#familyGatheringParagraph").html("Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you We’ll be sure to mark your special date with an unforgettable meal.")
-        $("#familyGatheringImg").attr("src", "images/homepage/special-events-desktop.jpg")
-        $(this).css('color', 'black');
-        $('#familyGathering1').css('color', '#4C4C4C');
-        $('#familyGathering3').css('color', '#4c4c4c');
-        $("#section3HR").css({top: '34%', right: '107%', position:'absolute'});
-    } else {
-        $("#familyGatheringHeader").html("Social Events")
-        $("#familyGatheringParagraph").html("Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.")
-        $("#familyGatheringImg").attr("src", "images/homepage/social-events-desktop.jpg")
-        $(this).css('color', 'black');
-        $('#familyGathering1').css('color', '#4C4C4C');
-        $('#familyGathering2').css('color', '#4c4c4c');
-        $("#section3HR").css({top: '70%', right: '107%', position:'absolute'});
-    }
-});
+    $('#familyGatheringBtn, #specialEventsBtn, #socialEventsBtn').click(function () {
+        if (this.id == 'familyGatheringBtn') {
+            $("#familyGatheringHeader").html("Family Gathering")
+            $("#familyGatheringParagraph").html("We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We'll provide a memorable experience for all.")
+            $('#specialEvents').css('display', 'none');
+            $('#socialEvents').css('display', 'none');
+            $('#familyGathering').css('display', 'block');
+            $(this).css('color', 'black');
+            $('#specialEventsBtn').css('color', '#4C4C4C');
+            $('#socialEventsBtn').css('color', '#4c4c4c');
+            $("#section3HR").css({ top: '-2%', right: '107%', position: 'absolute' });
+        } else if (this.id == 'specialEventsBtn') {
+            $("#familyGatheringHeader").html("Special Events")
+            $("#familyGatheringParagraph").html("Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you We’ll be sure to mark your special date with an unforgettable meal.")
+            $('#familyGathering').css('display', 'none');
+            $('#socialEvents').css('display', 'none');
+            $('#specialEvents').css('display', 'block');
+            $('#specialEvents').css('position', 'relative');
+            $(this).css('color', 'black');
+            $('#familyGatheringBtn').css('color', '#4C4C4C');
+            $('#socialEventsBtn').css('color', '#4c4c4c');
+            $("#section3HR").css({ top: '34%', right: '107%', position: 'absolute' });
+        } else {
+            $("#familyGatheringHeader").html("Social Events")
+            $("#familyGatheringParagraph").html("Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.")
+            $('#familyGathering').css('display', 'none');
+            $('#specialEvents').css('display', 'none');
+            $('#socialEvents').css('display', 'block');
+            $('#socialEvents').css('position', 'relative');
+            $(this).css('color', 'black');
+            $('#familyGatheringBtn').css('color', '#4C4C4C');
+            $('#specialEventsBtn').css('color', '#4c4c4c');
+            $("#section3HR").css({ top: '70%', right: '107%', position: 'absolute' });
+        }
+    });
 
 // BOOKING.HTML
 
